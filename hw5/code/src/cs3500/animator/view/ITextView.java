b9e670eb2bb97ef21268b299e8a1c60896b85455
meghanfahy <fahy.m@husky.neu.edu>
2019-03-19 19:25:47 -0400

    Update ITextView.java
    tests

--- hw5/code/src/cs3500/animator/view/ITextView.java
+++ hw5/code/src/cs3500/animator/view/ITextView.java
@@ -1,8 +1,16 @@
 package cs3500.animator.view;
 
+/**
+ * This is the interface for the textual view.  It extends the IView interface so that all three
+ * views are effectively interchangeable.
+ */
 public interface ITextView extends IView {
 
-  public void render() throws IllegalArgumentException;
+  /**
+   * Generates the output for the textView. Appends the output to the desired appendable.
+   * @throws IllegalArgumentException if unable to append to the appendable.
+   */
+  void render() throws IllegalArgumentException;
 
 
 }
