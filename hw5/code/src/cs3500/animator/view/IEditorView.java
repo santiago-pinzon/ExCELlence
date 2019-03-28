60a7863670914537582716e48eaf36b7c970848e
santiago-pinzon <38019518+santiago-pinzon@users.noreply.github.com>
2019-03-28 14:04:35 -0400

    Fixed merge conflicts

--- hw5/code/src/cs3500/animator/view/IEditorView.java
+++ hw5/code/src/cs3500/animator/view/IEditorView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package cs3500.animator.view;
 
 public interface IEditorView extends IView {
@@ -7,13 +6,4 @@ public interface IEditorView extends IView {
 
   public void setVisible();
 }
-=======
-package cs3500.animator.view;
 
-public interface IEditorView extends IView {
-
-  public void refresh();
-
-  public void setVisible();
-}
->>>>>>> 8767b4cd8a89ceff3fc8d2fb8c2c67c90c1dcbb6
